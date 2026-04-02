f9994c2a8fe87990c29e76a644ead4d0e3e3303b
asenetcky <me@asenetcky.dev>
2026-04-02 12:28:05 -0400

    feat: add qol for me

--- R/main.R
+++ R/main.R
@@ -1,3 +1,14 @@
-main <- function() cli::cli_alert_info("Hello from r-development-container!")
+main <- function() {
+  cli::cli_alert_info("Hello from r-development-container!")
+  cli::cli_alert_success(
+    glue::glue(
+      "
+      \n
+      Look I'm in a container here: '{fs::path_wd()}' 
+      and I'm running on {sessionInfo()$running}
+      "
+    )
+  )
+}
 
 main()
